b904cd9b3bd932cb62ed4ee547067fdb13ff9274
MANI SAI TEJA BEJJALA <manisaiteja2007@gmail.com>
2025-11-20 21:14:45 +0530

    jjh

--- utils/config/comment_prefixes.py
+++ utils/config/comment_prefixes.py
@@ -51,6 +51,7 @@ def get_comment_prefix(ext: str) -> str:
         "kt": "// ",
         "scala": "// ",
         "m": "% ",
+        "ino": "// ",
         
         # Multi-line comment openers
         "css": "/* ",
